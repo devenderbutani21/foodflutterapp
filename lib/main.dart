e9178a9814a5ef5dfafad066bbb31d33c490dbae
devenderbutani21 <devenderbutani21@gmail.com>
2020-08-26 22:33:07 +0530

    Http Changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import './widgets/list_item_types.dart';
+import './model/project_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,72 +32,98 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Map data;
 
-  Future<void> getData() async {
-    http.Response response =
-        await http.Client().get('http://10.0.2.2:5000/myData');
+  Future getProjectDetails() async {
+//    http.Response response =
+//        await http.Client().get('http://10.0.2.2:5000/myData');
+//    this.data = jsonDecode(response.body);
+    http.Response response = await http.get('http://10.0.2.2:5000/myData');
     this.data = jsonDecode(response.body);
     print(data);
+    return data;
+  }
+
+  Widget projectWidget() {
+    return FutureBuilder(
+      builder: (context, projectSnap) {
+        if (projectSnap.connectionState == ConnectionState.none &&
+            projectSnap.hasData == null) {
+          return Container();
+        }
+        return ListView.builder(
+          itemCount: projectSnap.data.length,
+          itemBuilder: (context, index) {
+            ProjectModel project = projectSnap.data[index];
+            return Column(
+              children: <Widget>[],
+            );
+          },
+        );
+      },
+      future: getProjectDetails(),
+    );
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getProjectDetails();
   }
 
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
     return SafeArea(
-        child: Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                height: deviceSize.height,
-                width: deviceSize.width / 3,
-                color: Color(0xffFD6A02),
-              ),
-              Container(
-                height: deviceSize.height,
-                width: deviceSize.width * 2 / 3,
-                color: Color(0xfff9f8f6),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              SizedBox(
-                width: 40,
-              ),
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {},
-                color: Colors.white,
-                iconSize: 25.0,
-              ),
-              SizedBox(
-                width: 250,
-              ),
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-                color: Colors.grey.shade400,
-                iconSize: 25.0,
-              ),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              SizedBox(
-                height: 90,
-              ),
-              ListItemTypes(data['1']['category'], 'Yellow', 'Mate'),
-            ],
-          ),
-        ],
+      child: Scaffold(
+        body: projectWidget(),
+//        body: Stack(
+//          children: <Widget>[
+//            Row(
+//              children: <Widget>[
+//                Container(
+//                  height: deviceSize.height,
+//                  width: deviceSize.width / 3,
+//                  color: Color(0xffFD6A02),
+//                ),
+//                Container(
+//                  height: deviceSize.height,
+//                  width: deviceSize.width * 2 / 3,
+//                  color: Color(0xfff9f8f6),
+//                ),
+//              ],
+//            ),
+//            Row(
+//              children: <Widget>[
+//                SizedBox(
+//                  width: 40,
+//                ),
+//                IconButton(
+//                  icon: Icon(Icons.menu),
+//                  onPressed: () {},
+//                  color: Colors.white,
+//                  iconSize: 25.0,
+//                ),
+//                SizedBox(
+//                  width: 250,
+//                ),
+//                IconButton(
+//                  icon: Icon(Icons.shopping_cart),
+//                  onPressed: () {},
+//                  color: Colors.grey.shade400,
+//                  iconSize: 25.0,
+//                ),
+//              ],
+//            ),
+//            Column(
+//              children: <Widget>[
+//                SizedBox(
+//                  height: 90,
+//                ),
+//                ListItemTypes(data['1']['category'], 'Yellow', 'Mate'),
+//              ],
+//            ),
+//          ],
+//        ),
       ),
-    ));
+    );
   }
 }
---
+++ lib/model/project_model.dart
@@ -0,0 +1,13 @@
+class ProjectModel {
+  String category;
+  String imgUrl;
+  String items;
+  List menu;
+
+  ProjectModel({
+    this.category,
+    this.imgUrl,
+    this.items,
+    this.menu,
+  });
+}
